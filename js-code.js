58f78bca3ac1d7f6ecbae9ee27e0e35c5872ae12
Allen Zhang <37892968+zhangtao25@users.noreply.github.com>
2023-08-31 23:40:31 +0800

    feat: upd

---
+++ js-code.js
@@ -0,0 +1,123 @@
+const data = {
+    "fileDetail": {
+        "file_name": "Welcome.tsx",
+        "file_path": "src/pages/Welcome.tsx",
+        "size": 219,
+        "encoding": "base64",
+        "content_sha256": "7145e4fab4e9aa1f53323d4e80c9594a3dc0d3841107cf9e3618aa4c01c8eddd",
+        "ref": "506c94606545a3fac265339550c7d550214a6822",
+        "blob_id": "7241d510f5c1defec4cbaeda836c98cd8b8f7e49",
+        "commit_id": "506c94606545a3fac265339550c7d550214a6822",
+        "last_commit_id": "7a77434c15164b7ace6678c7c3d25fa2df1b3912",
+        "content": "aW1wb3J0IHt1c2VOYXZpZ2F0ZX0gZnJvbSAicmVhY3Qtcm91dGVyLWRvbSI7Cgpjb25zdCBXZWxjb21lID0gKCkgPT4gewpjb25zdCBuYXZpZ2F0ZSA9IHVzZU5hdmlnYXRlKCkKcmV0dXJuIDxkaXY+CiAgICA8YnV0dG9uIG9uQ2xpY2s9eygpPT57CiAgICAgICAgbmF2aWdhdGUoLTEpCiAgICB9fT7lm57ljrs8L2J1dHRvbj4KPC9kaXY+Cn0KCmV4cG9ydCBkZWZhdWx0IFdlbGNvbWUK"
+    },
+    "fileCoverage": {
+        "path": "src/pages/Welcome.tsx",
+        "statementMap": [
+            {
+                "start": {
+                    "line": 3,
+                    "column": 16
+                },
+                "end": {
+                    "line": 10,
+                    "column": 1
+                }
+            },
+            {
+                "start": {
+                    "line": 4,
+                    "column": 17
+                },
+                "end": {
+                    "line": 4,
+                    "column": 30
+                }
+            },
+            {
+                "start": {
+                    "line": 5,
+                    "column": 0
+                },
+                "end": {
+                    "line": 9,
+                    "column": 6
+                }
+            },
+            {
+                "start": {
+                    "line": 7,
+                    "column": 8
+                },
+                "end": {
+                    "line": 7,
+                    "column": 20
+                }
+            }
+        ],
+        "fnMap": [
+            {
+                "name": "(anonymous_0)",
+                "decl": {
+                    "start": {
+                        "line": 3,
+                        "column": 16
+                    },
+                    "end": {
+                        "line": 3,
+                        "column": 17
+                    }
+                },
+                "loc": {
+                    "start": {
+                        "line": 3,
+                        "column": 22
+                    },
+                    "end": {
+                        "line": 10,
+                        "column": 1
+                    }
+                },
+                "line": 3
+            },
+            {
+                "name": "(anonymous_1)",
+                "decl": {
+                    "start": {
+                        "line": 6,
+                        "column": 21
+                    },
+                    "end": {
+                        "line": 6,
+                        "column": 22
+                    }
+                },
+                "loc": {
+                    "start": {
+                        "line": 6,
+                        "column": 25
+                    },
+                    "end": {
+                        "line": 8,
+                        "column": 5
+                    }
+                },
+                "line": 6
+            }
+        ],
+        "branchMap": [],
+        "s": [
+            1,
+            0,
+            0,
+            0
+        ],
+        "f": [
+            0,
+            0
+        ],
+        "b": [],
+        "_coverageSchema": "1a1c01bbd47fc00a2c39e90264f33305004495a9",
+        "hash": "44a06ed36306fbbfd189234db0541dd6d0488999"
+    }
+}
